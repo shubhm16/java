73614d9fc40327d07d75a23106f2849ce19e0882
shubham_mohite <shubham_mohite@vistasoftwares.com>
2020-02-13 16:30:48 +0530

    Hello World java program

---
+++ task-1/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld{
+    public static void main (String[] abc) {
+        System.out.println("Hello World");
+        System.out.println("args 1"+ abc[0]);
+        System.out.println("args 2"+ abc[1]);
+        System.out.println("args 3"+ abc[2]);
+    }
+    
+}
