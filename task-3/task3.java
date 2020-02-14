5ae7389ca96d52b7e58fedf2ef8b38e86d5d868e
shubham_mohite <shubham_mohite@vistasoftwares.com>
2020-02-14 18:05:14 +0530

    task3 complete

---
+++ task-3/task3.java
@@ -0,0 +1,15 @@
+public class task3 {
+	public static void print(int n) {
+		if (n <= 10) {
+		System.out.println(n);
+		print(n + 1);
+		}
+	}
+ 
+	public static void main(String[] args) {
+		print(1);
+ 
+	}
+ 
+}
+ 
